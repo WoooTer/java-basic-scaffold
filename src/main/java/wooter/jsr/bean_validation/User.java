a4bbc01a183dd0a0f10caaf8d1cafce432793390
WooTer <wutao1155@qq.com>
2020-04-20 15:16:34 +0800

    add jsr380 Bean Validation eg.

---
+++ src/main/java/wooter/jsr/bean_validation/User.java
@@ -0,0 +1,87 @@
+package wooter.jsr.bean_validation;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import javax.validation.constraints.*;
+import java.util.Set;
+
+/**
+ * https://www.baeldung.com/javax-validation
+ */
+public class User {
+
+    public static void main(String[] args) {
+        User user = new User();
+        user.setWorking(true);
+        user.setAboutMe("Its all about me!");
+        user.setAge(180);
+
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+        for (ConstraintViolation<User> violation : violations) {
+            System.out.println(violation.getMessage());
+        }
+    }
+
+    @NotNull(message = "Name cannot be null")
+    private String name;
+
+    @AssertTrue
+    private boolean working;
+
+    @Size(min = 10, max = 200, message
+            = "About Me must be between 10 and 200 characters")
+    private String aboutMe;
+
+    @Min(value = 18, message = "Age should not be less than 18")
+    @Max(value = 150, message = "Age should not be greater than 150")
+    private int age;
+
+    @Email(message = "Email should be valid")
+    private String email;
+
+    // standard setters and getters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isWorking() {
+        return working;
+    }
+
+    public void setWorking(boolean working) {
+        this.working = working;
+    }
+
+    public String getAboutMe() {
+        return aboutMe;
+    }
+
+    public void setAboutMe(String aboutMe) {
+        this.aboutMe = aboutMe;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
